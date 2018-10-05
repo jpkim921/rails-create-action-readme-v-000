6f5126847a3e27026b428c3478cc0c509e4915ed
Jayon Kim <jpkim921@gmail.com>
2018-10-05 03:00:31 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,17 @@ class PostsController < ApplicationController
 
 	def new
 		@post = Post.new
+		# redirect_back(fallback_location: new_post_path)
 	end
 
 	def create
-		@post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+
+		if @post = Post.create(title: params[:title], description: params[:description])
+			redirect_to post_path(@post)
+		else
+			redirect_back(fallback_location: new_post_path)
+		end
 	end
+
+
 end
